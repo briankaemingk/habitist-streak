dbb9456e07881a50f55c9782407684196726bb41
akeslo <akeslo@users.noreply.github.com>
2017-10-12 11:28:39 -0400

    Update habits.py

--- habits.py
+++ habits.py
@@ -29,7 +29,7 @@ def main():
         exit()
     user = todoist.login_with_api_token(API_TOKEN)
     project = user.get_project('Habbits')
-    tasks = project.add_filter('Overdue', todoist.Query.OVERDUE)
+    tasks = project.get_tasks()
     for task in tasks:
         content = task.content
         due = task.due_date_utc
