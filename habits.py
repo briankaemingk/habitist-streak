ce65159f2b897e7cb955e683a501a93e8d1ac54f
akeslo <akeslo@users.noreply.github.com>
2017-10-12 12:08:01 -0400

    Update habits.py

--- habits.py
+++ habits.py
@@ -49,7 +49,9 @@ def main():
             streak = int(habit.group(1)) + 1
             update_streak(task, streak)
         
-
+        if habit and today > due and complete == 0:
+            task.date_string = 'ev day starting tod'
+            update_streak(task, 0)
             
 if __name__ == '__main__':
     main()
