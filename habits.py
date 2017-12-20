fa0c2ab4eed790ba7a18b855bb841276bf951c51
Amit <meamitkc@gmail.com>
2017-12-20 21:29:00 +0545

    Fix deprecated pytodoist

--- habits.py
+++ habits.py
@@ -1,7 +1,8 @@
 import os
 import re
 import logging
-from pytodoist import todoist
+from datetime import datetime, timedelta
+from todoist.api import TodoistAPI
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -16,10 +17,20 @@ def is_habit(text):
     return re.search(r'\[day\s(\d+)\]', text)
 
 
-def update_streak(task, streak):
+def is_today(text):
+    today = datetime.utcnow().strftime("%a %d %b")
+    return text[:10] == today
+
+
+def is_due(text):
+    yesterday = (datetime.utcnow() - timedelta(1)).strftime("%a %d %b")
+    return text[:10] == yesterday
+
+
+def update_streak(item, streak):
     days = '[day {}]'.format(streak)
-    task.content = re.sub(r'\[day\s(\d+)\]', days, task.content)
-    task.update()
+    text = re.sub(r'\[day\s(\d+)\]', days, item['content'])
+    item.update(content=text)
 
 
 def main():
@@ -27,20 +38,24 @@ def main():
     if not API_TOKEN:
         logging.warn('Please set the API token in environment variable.')
         exit()
-    user = todoist.login_with_api_token(API_TOKEN)
-    tasks = user.search_tasks(todoist.Query.TODAY)
+    api = TodoistAPI(API_TOKEN)
+    api.sync()
+    tasks = api.state['items']
     for task in tasks:
-        habit = is_habit(task.content)
-        if habit:
-            streak = int(habit.group(1)) + 1
-            update_streak(task, streak)
-
-    tasks = user.search_tasks(todoist.Query.OVERDUE)
-    for task in tasks:
-        habit = is_habit(task.content)
-        if habit:
-            task.date_string = 'ev day starting tod'
-            update_streak(task, 0)
+        if task['due_date_utc']:
+            if is_today(task['due_date_utc']):
+                habit = is_habit(task['content'])
+                if habit:
+                    streak = int(habit.group(1)) + 1
+                    print(streak)
+                    print(task['content'])
+                    update_streak(task, streak)
+            elif is_due(task['due_date_utc']):
+                habit = is_habit(task['content'])
+                if habit:
+                    update_streak(task, 0)
+                    task.update(date_string='ev day starting tod')
+    api.commit()
 
 if __name__ == '__main__':
     main()
