359371d3666bc6779facac6c50706d394d74c209
Akeslo <akeslo@kesdev.space>
2017-12-26 08:16:51 -0500

    Logging

--- habits.py
+++ habits.py
@@ -44,6 +44,7 @@ def main():
     api.sync()
     tasks = api.state['items']
     for task in tasks:
+        print task
         if task['due_date_utc'] and is_habit(task['content']):
             if is_today(task['due_date_utc']):
                 habit = is_habit(task['content'])
