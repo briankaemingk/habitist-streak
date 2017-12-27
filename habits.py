4419e20b2b747787c33b39f5c132eec529b4de2e
Akeslo <akeslo@kesdev.space>
2017-12-26 23:24:38 -0500

    Logging

--- habits.py
+++ habits.py
@@ -46,7 +46,7 @@ def main():
     tasks = api.state['items']
     for task in tasks:
         if task['due_date_utc'] and is_habit(task['content']):
-            print(due_date_utc);
+            print(task['due_date_utc');
             if is_today(task['due_date_utc']):
                 habit = is_habit(task['content'])
                 streak = int(habit.group(1)) + 1
