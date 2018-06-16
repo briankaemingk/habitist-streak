6b354979c720f41bcfa87c57bec896bb584d8c03
Brian E Kaemingk <brian.e.k@gmail.com>
2018-06-15 23:12:13 -0700

    Testing body parse

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-from flask import Flask
+from flask import Flask, request
 import habits
 
 app = Flask(__name__)
 
 
-@app.route('/task/<taskurl>')
-def main(taskurl):
-    habits.main(taskurl)
+@app.route('/task/<task_content>')
+def main(task_content):
+    habits.main(task_content)
+    data = request.data
+    print (data)
 
-    return taskurl
+    return task_content
 
 
 if __name__ == '__main__':
